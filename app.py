1ad19d73c542b84951acd0e21fe598703e8a44e3
符志成 Kevin <83894017+kevinfu1717@users.noreply.github.com>
2024-03-14 23:28:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import gradio as gr
 from promptsBase import *
-from internlmXcomposer import predict, model,defaultImage
 import time
 import os
 
+import os
+os.system("mkdir Shanghai_AI_Laboratory")
+os.system("wget -O internlm-xcomposer2-7b.zip https://bj.bcebos.com/v1/ai-studio-online/a555f79d63e34a4b85c6c5edb23305a2843ff32171554c90af8048301f8d1eb7?responseContentDisposition=attachment%3B%20filename%3Dinternlm-xcomposer2-7b.zip&authorization=bce-auth-v1%2F5cfe9a5e1454405eb2a975c43eace6ec%2F2024-03-14T14%3A50%3A42Z%2F-1%2F%2F377d77f7224b1cb96a69aa8de8df0867a08654d90acf04e862cbe4b628d236f2")
+os.system("ls")
+os.system("unzip internlm-xcomposer2-7b.zip -d Shanghai_AI_Laboratory/")
+os.system("pip install modelscope")
+os.system("pip install modelscope[multi-modal]")
+os.system("pip install torch==2.1.2 torchvision")
+
+from internlmXcomposer import predict, model,defaultImage
 
 def clear_fn(value):
     return "",None, "请上传儿童插画的照片，并输入故事主题"
